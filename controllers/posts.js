bf740bfde417f9cdc52273643050e7c5cb7e396a
MannySanz <mannysanz715@gmail.com>
2023-01-06 23:42:55 -0800

    Create routes for show edit post. create delete and update

--- controllers/posts.js
+++ controllers/posts.js
@@ -17,8 +17,65 @@ function newPost(req,res){
   res.render('posts/new')
 }
 
+function create(req, res){
+  console.log(req.body)
+  Post.create(req.body)
+  .then(post =>{
+    res.redirect('/blog')
+  })
+  .catch(err =>{
+    console.log(err)
+    res.render('posts/index')
+  })
+}
+
+function show(req, res){
+  console.log(req.params.id)
+  Post.findById(req.params.id)
+  .then( post =>{
+    res.render('posts/show', {
+      post
+    })
+    
+  }).catch(err =>{
+      console.log(err)
+      res.render('posts/index')
+    })
+}
+
+function deletePost(req, res){
+  Post.findByIdAndDelete(req.params.id)
+  .then( post =>{
+    res.redirect('/blog')
+  })
+  .catch(err =>{
+    console.log(err)
+    res.render('posts/index')
+  })
+}
+
+function edti(req, res){
+  
+}
+
+function update(req, res){
+  Post.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  .then(post =>{
+    res.redirect('posts/index')
+  })
+  .catch(err =>{
+    console.log(err)
+    res.render('posts/index')
+  })
+}
+
 export{
   index,
   newPost as new,
-  
+  create,
+  show,
+  deletePost as delete,
+  edit,
+  update
+
 }
--- routes/posts.js
+++ routes/posts.js
@@ -4,10 +4,21 @@ const router = Router()
 
 
 /* GET users listing. */
+
 router.get('/', postsCtrl.index)
 
 router.get('/new', postsCtrl.new)
 
+router.get('/:id', postsCtrl.show)
+
+router.get('/:id/edit', postsCtrl.edit)
+
+router.post('/', postsCtrl.create)
+
+router.delete('/:id', postsCtrl.delete)
+
+
+router.put('/:id', postsCtrl.update)
 export {
   router
 }
